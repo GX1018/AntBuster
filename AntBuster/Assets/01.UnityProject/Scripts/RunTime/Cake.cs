6d7a9e9532fd243ecb304a9721dd694e2f64c218
GX1018 <wjdghlxo@gmail.com>
2023-02-06 23:03:39 +0900

    0206End

--- AntBuster/Assets/01.UnityProject/Scripts/RunTime/Cake.cs
+++ AntBuster/Assets/01.UnityProject/Scripts/RunTime/Cake.cs
@@ -5,24 +5,34 @@ using UnityEngine.UI;
 
 public class Cake : MonoBehaviour
 {
-    public int pieceOfCake = 8;
+    public int pieceOfCake = default;
+
+    Sprite[] sprites;
+
+    Image curruntImage;
 
     
     // Start is called before the first frame update
     void Start()
     {
+        sprites = Resources.LoadAll<Sprite>("Sprites/cake");
+        pieceOfCake= 8;
     }
 
     // Update is called once per frame
     void Update()
     {
+        Debug.Log(sprites[8]);
+        //curruntImage.GetComponent<Image>().sprite = sprites[8];
+        Debug.Log(pieceOfCake);
+
     }
 
-    private void OnTriggerEnter2D(Collider2D other) 
-    {
-        if(pieceOfCake>0)
+    private void OnTriggerEnter2D(Collider2D other) {
+        if(other.tag == "Ant")
         {
-            pieceOfCake --;
+            pieceOfCake--;
+            other.GetComponent<GameObject>().SetActive(false);
         }
     }
 }
